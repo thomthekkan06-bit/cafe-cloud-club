dc3f15cbe943a15de07b6c4b2160e1c0680e9f5b
thomthekkan06-bit <thomthekkan06@gmail.com>
2025-12-21 20:21:06 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -821,7 +821,7 @@ window.finalizeOrder = function() {
                 return;
             }
         }
-        const mapLink = `https://www.google.com/maps/search/?api=1&query=$${lat},${lng}`;
+        const mapLink = https://www.google.com/maps/search/?api=1&query=${lat},${lng};
         address = `${house}, ${street}\n(Landmark: ${landmark})\n📍 Pin: ${mapLink}`;
     }
 
